a380c73e4ae58f1803d551b8a05f6b0c7fa77158
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-07-31 09:46:39 +0700

    FRONT-9 update_productList

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Categories.module.css';
+type Props = {}
+
+const Categories = (props: Props) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Categories
