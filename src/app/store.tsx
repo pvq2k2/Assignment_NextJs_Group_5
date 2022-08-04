588d26f95f95607a3a1f374b2f4aab2c38b93f73
pvq2k2 <vanquyet2392002e@gmail.com>
2022-08-04 21:47:06 +0700

    FRONT-54 feature: Create slider slice

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -4,12 +4,14 @@ import productReducer from "../features/product/product.slice";
 import categoryReducer from "../features/category/category.slice";
 import userReducer from "../features/user/user.slice";
 import authReducer from "../features/auth/auth.slice";
+import slideReducer from "../features/slide/slide.slice";
 export const store = configureStore({
     reducer: {
         product: productReducer,
         category: categoryReducer,
         user: userReducer,
-        auth: authReducer
+        auth: authReducer,
+        slide: slideReducer,
     },
 });
 
