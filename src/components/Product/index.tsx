860f65b35ce24ecf4d53a8e6d18af86a993603d2
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-08-04 22:25:00 +0700

    FRONT-42 update function-for-component-product

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
+import { type } from 'os';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { IProduct } from '../../models/product';
 // import { getAllP } from '../../features/product/product.slide'
-import styles from './Products.module.css'
-const Products = () => {
+import styles from './Products.module.css';
+type ProductProps={
+  listProducts:IProduct[];
+}
+const Products = ({listProducts}:ProductProps) => {
   // const product= useSelector((state: any) => state.product.products)
   // const dispatch  = useDispatch<any>();
   // console.log(product);
@@ -20,7 +25,7 @@ const Products = () => {
           <h2>WEEKLY BEST</h2>
         </div>
         <div className={styles.product}>
-          {/* {product.map((item,index)=>(
+          {listProducts.map((item,index)=>(
             <div className={styles.item} key={index}>
            <div className={styles.tranf}>
               <div className={styles.item_image}>
@@ -40,7 +45,7 @@ const Products = () => {
 
             </div>
           </div>
-          ))} */}
+          ))}
         </div>
 
       </div>
