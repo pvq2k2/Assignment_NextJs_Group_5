7410fceede0d59c613a1b3a2daa6039455e660c7
pvq2k2 <vanquyet2392002e@gmail.com>
2022-08-07 22:37:07 +0700

    FRONT-56 feature: Create cart slice - Create api cart

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -5,6 +5,7 @@ import categoryReducer from "../features/category/category.slice";
 import userReducer from "../features/user/user.slice";
 import authReducer from "../features/auth/auth.slice";
 import slideReducer from "../features/slide/slide.slice";
+import cartReducer from "../features/slide/slide.slice";
 export const store = configureStore({
     reducer: {
         product: productReducer,
@@ -12,6 +13,7 @@ export const store = configureStore({
         user: userReducer,
         auth: authReducer,
         slide: slideReducer,
+        cart: cartReducer
     },
 });
 
